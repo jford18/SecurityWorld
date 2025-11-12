4e1b59792d03c2fb5b7d2e4c568e3d038166347c
Jackson Ford <jackson.ford18@gmail.com>
2025-11-11 21:43:13 -0500

    chan

--- server/controllers/tipoArea.controller.js
+++ server/controllers/tipoArea.controller.js
@@ -1,11 +1,19 @@
-// controllers/tipoArea.controller.js
 import db from "../db.js";
 
 export const getTipoArea = async (req, res) => {
   try {
-    const result = await db.query('SELECT id, nombre FROM public."tipo_Area" WHERE activo = true ORDER BY nombre ASC;');
+    const result = await db.query(`
+      SELECT id, nombre 
+      FROM public.tipo_area 
+      WHERE activo = true 
+      ORDER BY nombre ASC;
+    `);
     res.json(result.rows);
-  } catch (error) '{"error": "[API][ERROR] /api/tipo_area:", "details": error}'
-    res.status(500).json({ message: "Error al obtener tipos de área" });
+  } catch (error) {
+    console.error("[API][ERROR] /api/tipo_area:", error);
+    res.status(500).json({ 
+      error: "[API][ERROR] /api/tipo_area", 
+      details: error.message 
+    });
   }
 };
