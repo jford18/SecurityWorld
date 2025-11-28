0997f2eafb8ab27c0e4a48a7e2a429060809c5a0
jford18 <67157718+jford18@users.noreply.github.com>
2025-11-28 14:34:35 -0500

    Remove auth middleware from menu CRUD routes

--- server/routes/menu.routes.js
+++ server/routes/menu.routes.js
@@ -11,11 +11,11 @@ import verifyToken from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", verifyToken, listMenus);
+router.get("/", listMenus);
 router.get("/authorized", verifyToken, getMenusByRole);
-router.get("/:id", verifyToken, getMenuById);
-router.post("/", verifyToken, createMenu);
-router.put("/:id", verifyToken, updateMenu);
-router.delete("/:id", verifyToken, deleteMenu);
+router.get("/:id", getMenuById);
+router.post("/", createMenu);
+router.put("/:id", updateMenu);
+router.delete("/:id", deleteMenu);
 
 export default router;
